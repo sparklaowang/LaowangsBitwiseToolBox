7ba58776af2e0afb5cb2c3e8e7ee375d0aaa5cac
wzyphy <wzyphy@mail.ustc.edu.cn>
2020-06-02 21:16:46 +0800

    Seperate definition of CONVERTER to a single file

---
+++ cvt.h
@@ -0,0 +1,13 @@
+#ifndef CVT_H
+#define CVT_H
+typedef union union_muyltitype_memprintf { 
+    uint8_t u8;
+    uint16_t u16;
+    uint32_t u32;
+    int8_t i8;
+    int16_t i16;
+    int32_t i32;
+    double db;
+    float sg;
+} CONVERTER;
+#endif
